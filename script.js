0f397ca85a84725ce4df24ccd7ac528c56e7baca
Your Name <you@example.com>
2023-11-24 07:18:49 +0100

    show card content

--- script.js
+++ script.js
@@ -1,2 +1,82 @@
+let currentQuestion = 0;
+
+
 // Create your own JSON-Array
 // Check your Array with JSON Array Formatter and Validator
+
+let questions = [
+    {
+        'question': 'Ist das meine erste Frage?',
+        'answer1': 'Das ist Antwort 1.',
+        'answer2': 'Das ist Antwort 2.',
+        'answer3': 'Das ist Antwort 3.',
+        'answer4': 'Das ist Antwort 4.',
+        'answer': 'Das ist die richtige Antwort.'
+    },
+    {
+        'question': 'Frage',
+        'answer1': 'Antwort1',
+        'answer2': 'Antwort2',
+        'answer3': 'Antwort3',
+        'answer4': 'Antwort4',
+        'answer': 'Antwort'
+    },
+    {
+        'question': 'Frage',
+        'answer1': 'Antwort1',
+        'answer2': 'Antwort2',
+        'answer3': 'Antwort3',
+        'answer4': 'Antwort4',
+        'answer': 'Antwort'
+    },
+    {
+        'question': 'Frage',
+        'answer1': 'Antwort1',
+        'answer2': 'Antwort2',
+        'answer3': 'Antwort3',
+        'answer4': 'Antwort4',
+        'answer': 'Antwort'
+    },
+    {
+        'question': 'Frage',
+        'answer1': 'Antwort1',
+        'answer2': 'Antwort2',
+        'answer3': 'Antwort3',
+        'answer4': 'Antwort4',
+        'answer': 'Antwort'
+    },
+    {
+        'question': 'Frage',
+        'answer1': 'Antwort1',
+        'answer2': 'Antwort2',
+        'answer3': 'Antwort3',
+        'answer4': 'Antwort4',
+        'answer': 'Antwort'
+    },
+    {
+        'question': 'Frage',
+        'answer1': 'Antwort1',
+        'answer2': 'Antwort2',
+        'answer3': 'Antwort3',
+        'answer4': 'Antwort4',
+        'answer': 'Antwort'
+    }
+];
+
+
+function init() {
+    document.getElementById('all-questions').innerHTML = questions.length;
+
+    showQuestion();
+}
+
+
+function showQuestion() {
+    let question = questions[currentQuestion];
+
+    document.getElementById('question').innerHTML = question['question'];
+    document.getElementById('answer1').innerHTML = question['answer1'];
+    document.getElementById('answer2').innerHTML = question['answer2'];
+    document.getElementById('answer3').innerHTML = question['answer3'];
+    document.getElementById('answer4').innerHTML = question['answer4'];
+}
